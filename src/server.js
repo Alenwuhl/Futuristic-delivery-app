361486462ed7395e0dcef5377d8241e809f33f30
Alenwuhl <wuhlalen@gmail.com>
2024-04-14 12:49:05 -0300

    after twitter

--- src/server.js
+++ src/server.js
@@ -1,24 +1,15 @@
 import express from "express";
-import session from 'express-session'; 
 import cors from 'cors';
 import dotenv from 'dotenv';
 import authMiddleware from "./middlewares/authMiddleware.js";
 import path from 'path';
 import { __dirname } from "./utils.js";
-import passport from 'passport'; 
-import { Strategy as TwitterStrategy } from 'passport-twitter';
 import categoryRouter from './routes/category.route.js';
 import extraRouter from './routes/extra.route.js';
 import orderRouter from './routes/order.route.js';
 import productRouter from './routes/product.route.js';
-import twitterRouter from './routes/auth.twitter.js';
 
 dotenv.config();
-
-// Twitter keys
-const TWITTER_CONSUMER_KEY = process.env.TWITTER_CONSUMER_KEY;
-const TWITTER_CONSUMER_SECRET = process.env.TWITTER_CONSUMER_SECRET;
-const EXPRESS_SESSION_SECRET = process.env.EXPRESS_SESSION_SECRET;
 
 // Initialize Express app
 const app = express();
